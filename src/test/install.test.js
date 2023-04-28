a83b0facd83a1374a2fc398a97cf09917a1f9b68
Zane Rockenbaugh <zane@liquid-labs.com>
2023-04-28 10:14:53 -0500

    broke out common test setup to library

--- src/test/install.test.js
+++ src/test/install.test.js
@@ -1,39 +1,34 @@
 /* global describe expect test */
 // these tests must be executed serially
 import * as fs from 'node:fs/promises'
-import * as fsPath from 'node:path'
-
-import { tryExec } from '@liquid-labs/shell-toolkit'
 
 import { install } from '../install'
+import { setupTestPackage } from './test-setup-lib'
 
 describe('install', () => {
   test('installs new packages', async() => {
-    const testPath = fsPath.join(__dirname, 'pkgIn001')
-    try {
-      await fs.rm(testPath, { recursive : true })
-    }
-    catch (e) {
-      if (e.code !== 'ENOENT') throw e
-    }
-    await fs.mkdir(testPath)
-    tryExec(`cd ${testPath} && npm init -y`)
+    const { pkgPath, testPath } = await setupTestPackage({ pkgName : 'pkgIn001' })
+
     install({ pkgs : ['http-errors'], targetPath : testPath })
-    const pkgJSON = JSON.parse(await fs.readFile(fsPath.join(testPath, 'package.json'), { encoding : 'utf8' }))
+    const pkgJSON = JSON.parse(await fs.readFile(pkgPath, { encoding : 'utf8' }))
     expect(pkgJSON?.dependencies['http-errors']).toBeTruthy()
   })
 
+  // expects to be executed serially with the previous test
   test("'saveDev' switches package to devDependencies", async() => {
-    const testPath = fsPath.join(__dirname, 'pkgIn001')
+    const { pkgPath, testPath } = await setupTestPackage({ noReset : true, pkgName : 'pkgIn001' })
+
     install({ pkgs : ['http-errors'], saveDev : true, targetPath : testPath })
-    const pkgJSON = JSON.parse(await fs.readFile(fsPath.join(testPath, 'package.json'), { encoding : 'utf8' }))
+    const pkgJSON = JSON.parse(await fs.readFile(pkgPath, { encoding : 'utf8' }))
     expect(pkgJSON?.devDependencies['http-errors']).toBeTruthy()
   })
 
+  // expects to be executed seriallly with the previous test
   test("'saveProd' switches package to dependencies", async() => {
-    const testPath = fsPath.join(__dirname, 'pkgIn001')
+    const { pkgPath, testPath } = await setupTestPackage({ noReset : true, pkgName : 'pkgIn001' })
+
     install({ pkgs : ['http-errors'], saveProd : true, targetPath : testPath })
-    const pkgJSON = JSON.parse(await fs.readFile(fsPath.join(testPath, 'package.json'), { encoding : 'utf8' }))
+    const pkgJSON = JSON.parse(await fs.readFile(pkgPath, { encoding : 'utf8' }))
     expect(pkgJSON?.dependencies['http-errors']).toBeTruthy()
   })
 })
