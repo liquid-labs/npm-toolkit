c06c57c640b9c259cdd1e7b911b10ea0522d6d33
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-02 12:38:16 -0500

    implement support for installing local dev packages

--- src/install.mjs
+++ src/install.mjs
@@ -1,6 +1,33 @@
+import { existsSync } from 'node:fs'
+import * as fsPath from 'node:path'
+
 import { tryExec } from '@liquid-labs/shell-toolkit'
 
-const install = ({ global, latest, pkgs, saveDev, saveProd, targetPath, verbose, version }) => {
+const findLocalPackage = ({ devPaths, npmName }) => {
+    let [org, basename] = npmName.split('/')
+    if (basename === undefined) {
+      basename = org
+      org = undefined
+    }
+    else if (org.startsWith('@')) {
+      org = org.slice(1) // we will add back on later to test both
+    }
+    const pkgPath = org === undefined
+      ? fsPath.join(basename, 'package.json')
+      : fsPath.join(org, basename, 'package.json')
+    for (const devPath of devPaths) {
+      // TODO: this is a workaround until we transition fully to matching NPM names
+      for (const testPath of [fsPath.join(devPath, pkgPath), fsPath.join(devPath, '@' + pkgPath)]) {
+        if (existsSync(testPath)) {
+          return fsPath.dirname(testPath)
+        }
+      }
+    }
+
+    return null
+  }
+
+const install = ({ devPaths, global, latest, pkgs, saveDev, saveProd, targetPath, verbose, version }) => {
   if (pkgs === undefined || pkgs.length === 0) {
     throw new Error("No 'pkgs' specified; specify at least one package.")
   }
@@ -19,7 +46,17 @@ const install = ({ global, latest, pkgs, saveDev, saveProd, targetPath, verbose,
   const saveBit = saveDev === true ? '--save-dev ' : saveProd === true ? '--save-prod ' : ''
   const versionBit = latest === true ? '@latest' : version !== undefined ? '@' + version : ''
 
-  const installPkgs = pkgs.map((p) => p + versionBit).join(' ')
+  const installPkgs = pkgs
+    .map((p) => {
+      if (devPaths) {
+        const localPath = findLocalPackage({ devPaths, npmName: p })
+        if (localPath !== null) {
+          return localPath
+        }
+      }
+      return p + versionBit
+    })
+    .join(' ')
 
   const cmd = pathBit + 'npm install ' + globalBit + saveBit + installPkgs
   tryExec(cmd, { silent : !verbose })
