51108105beab2c49ef43c27a01ee458e1d271a3f
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-11 14:53:46 -0500

    fixed function name typo

--- src/get-package-org-and-basename.mjs
+++ src/get-package-org-and-basename.mjs
@@ -1,11 +1,10 @@
 import { getPackageJSON } from './get-package-json'
 
-const getPackageOrgAndBaseame = async({ pkgDir, pkgJSON }) => {
+const getPackageOrgAndBasename = async({ pkgDir, pkgJSON }) => {
   if (pkgDir !== undefined) {
     if (pkgJSON !== undefined) {
-      throw new Error("getPackageOrgAndBaseame: cannot specify both 'pkgJSON' and 'pkgDir'.")
+      throw new Error("getPackageOrgAndBasename: cannot specify both 'pkgJSON' and 'pkgDir'.")
     }
-    console.log('pkgDir:', pkgDir)
     pkgJSON = await getPackageJSON({ pkgDir })
   }
 
@@ -24,4 +23,4 @@ const getPackageOrgAndBaseame = async({ pkgDir, pkgJSON }) => {
   }
 }
 
-export { getPackageOrgAndBaseame }
+export { getPackageOrgAndBasename }
--- src/test/get-package-org-and-basename.test.js
+++ src/test/get-package-org-and-basename.test.js
@@ -1,23 +1,23 @@
 /* global describe expect test */
 import * as fsPath from 'node:path'
-import { getPackageOrgAndBaseame } from '../get-package-org-and-basename'
+import { getPackageOrgAndBasename } from '../get-package-org-and-basename'
 
-describe('getPackageOrgAndBaseame', () => {
+describe('getPackageOrgAndBasename', () => {
   test('handles a root-name package', async() => {
-    const { org, basename } = await getPackageOrgAndBaseame({ pkgJSON : { name : 'foo' } })
+    const { org, basename } = await getPackageOrgAndBasename({ pkgJSON : { name : 'foo' } })
     expect(org).toBe(undefined)
     expect(basename).toBe('foo')
   })
 
   test('handles an org scoped package', async() => {
-    const { org, basename } = await getPackageOrgAndBaseame({ pkgJSON : { name : '@acme/foo' } })
+    const { org, basename } = await getPackageOrgAndBasename({ pkgJSON : { name : '@acme/foo' } })
     expect(org).toBe('acme')
     expect(basename).toBe('foo')
   })
 
   test("will read from the filesystem when 'pkgDir' specified", async() => {
     const pkgDir = fsPath.join(__dirname, 'data', 'pkgA')
-    const { org, basename } = await getPackageOrgAndBaseame({ pkgDir })
+    const { org, basename } = await getPackageOrgAndBasename({ pkgDir })
     expect(org).toBe('acme')
     expect(basename).toBe('pkg-a')
   })
