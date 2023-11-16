c57e5cce5657edb2cad248c59faa3acb3042b389
Zane Rockenbaugh <zane@liquid-labs.com>
2023-11-16 10:31:45 -0600

    fix index export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export * from './get-package-json'
-export * from './get-package-org-and-basename'
+export * from './get-package-org-basename-and-version'
 export * from './install'
 export * from './update'
