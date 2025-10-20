215b24f116815de378f4c70515a35c6062ac9a3f
Zane Rockenbaugh <zane@liquid-labs.com>
2025-10-19 21:33:14 -0500

    improved and regularized results; updated libs; use @liquid-labs/semver-plus with explicit dependency

--- src/test/update.test.js
+++ src/test/update.test.js
@@ -16,7 +16,15 @@ describe('update', () => {
     const pkgJSON = JSON.parse(await fs.readFile(pkgPath, { encoding : 'utf8' }))
     pkgJSON.dependencies['http-errors'] = '^1.2.0'
 
-    update({ projectPath : testPath })
+    const { updated, actions, result } = await update({ projectPath : testPath })
+    expect(updated).toBe(true)
+    expect(actions.length).toBe(2) // update for 1.x and optional udpate to 2.x
+    expect(actions[0]).toMatch(/http-errors/)
+    expect(actions[0]).toMatch(/1\.8\.1/)
+    const httpErrorsChangeEntry = result.change.filter(({ from }) => from.name === 'http-errors')[0]
+    expect(httpErrorsChangeEntry.from.version).toBe('1.2.0')
+    expect(httpErrorsChangeEntry.to.version).toBe('1.8.1')
+
     const updateResult = tryExec(`cd ${testPath} && npm ls http-errors`)
     expect(updateResult.code).toBe(0)
     expect(updateResult.stdout.includes('http-errors@1.8.1')).toBe(true)
